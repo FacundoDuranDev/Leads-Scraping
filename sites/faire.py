7fb709120613a5da7cc50b64945ce3a086139d4a
FacundoDuranDev <facu.javi.du@gmail.com>
2021-08-17 22:31:36 -0300

    faire.py added instagram APi

--- sites/faire.py
+++ sites/faire.py
@@ -1,59 +1,41 @@
 import json
 import requests
 import yaml
-from yaml.loader import SafeLoader
+import re
 import time
+from yaml.loader import SafeLoader
 from utils.mongo import Mongo
 from utils.async_requests import _async_requests
 from utils.payload import Payload
 from utils.colors import colors
+from utils.private_keys import Private
+from InstagramAPI import InstagramAPI
 
 
 class Faire:
     def __init__(self,config,type,site):
         self.config = config['Sites']
         self.url = self.config[site]['url']
-        self.created_at = "2021-08-12" #time.strftime('%Y-%m-%d')
+        self.created_at = time.strftime('%Y-%m-%d')
         self.site = site
         self.mongo = Mongo()
         self.test_mongo = Mongo('testing')
         self.session = requests.session()
+        self.regex_time = re.compile(r'\d+\-\d+\-\d+')
         self.session.headers = {
         "cookie": "indigofair_session=eyJzZXNzaW9uX3Rva2VuIjoiNXIxZnd5Z2pld2VzNWQxd2x6Nnl6YmgwMDc0a3JzbTI0YnRzNmdjOThtcjZ4OHpoMDE1ODFoa3NubnhtYXNtYWJpa2psb3k3ZW45YWs3YTllaW03ajluN2kybjR5ajJ3dWViZiJ9--c1abc9583a014c005a5a1009bb2ef1ca9082080a3d2a753f69ebf4e6a1bd83582f9032e1605a134d41ebf818dd92d89ff67d324eff856e11265b715cd3477324; _ga=GA1.2.701631336.1628553791; _fbp=fb.1.1628553791262.465845606; _pin_unauth=dWlkPVpHRTJZVEUwTTJJdFlqRTJOUzAwTTJJekxUbG1abVV0Tm1Ga04yVmpOemhqWW1SaQ; _gcl_au=1.1.367746679.1628553792; hubspotutk=462d8801b9de513532c892b80fe20422; OptanonConsent=isIABGlobal=false&datestamp=Mon+Aug+16+2021+14%3A23%3A39+GMT-0300+(Argentina+Standard+Time)&version=6.17.0&hosts=&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0002%3A1%2CC0004%3A1&AwaitingReconsent=false&geolocation=AR%3BB; OptanonAlertBoxClosed=2021-08-16T17:23:39.651Z; _gid=GA1.2.65244427.1629134626; _gat_UA-90386801-1=1; _uetsid=b61c6480feb611ebbb8a31c3bf4e22b6; _uetvid=59dbebc0f96e11eb91d0cf6d2ec058f5; __hstc=62287155.462d8801b9de513532c892b80fe20422.1628553793504.1628730476917.1629134626519.7; __hssrc=1; __hssc=62287155.1.1629134626519"
         }
+        self.API = InstagramAPI(Private.USER,Private.PASSWORD)
+        self.API.login()
         self.scraped = self.mongo.query_field(field='Id',site=self.site,created_at=self.created_at)
-        self.headers = {
-        'authority': 'www.faire.com',
-        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"',
-        'x-faire-timezone': 'America/Buenos_Aires',
-        'sec-ch-ua-mobile': '?0',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
-        'x-if-client-release': '9f117660:a43393e9:0',
-        'content-type': 'application/json;charset=UTF-8',
-        'accept': 'application/json, text/plain, */*',
-        'x-if-wsat': '5r1fwygjewes5d1wlz6yzbh0074krsm24bts6gc98mr6x8zh01581hksnnxmasmabikjloy7en9ak7a9eim7j9n7i2n4yj2wuebf',
-        'dpr': '1',
-        'x-accept-currency': 'USD',
-        'x-if-app-identifier': 'web-retailer',
-        'x-is-apparel': 'false',
-        'origin': 'https://www.faire.com',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'referer': 'https://www.faire.com/category/Top%20Sellers',
-        'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
-        'cookie': 'indigofair_session=eyJzZXNzaW9uX3Rva2VuIjoiNXIxZnd5Z2pld2VzNWQxd2x6Nnl6YmgwMDc0a3JzbTI0YnRzNmdjOThtcjZ4OHpoMDE1ODFoa3NubnhtYXNtYWJpa2psb3k3ZW45YWs3YTllaW03ajluN2kybjR5ajJ3dWViZiJ9--c1abc9583a014c005a5a1009bb2ef1ca9082080a3d2a753f69ebf4e6a1bd83582f9032e1605a134d41ebf818dd92d89ff67d324eff856e11265b715cd3477324; _ga=GA1.2.701631336.1628553791; _gid=GA1.2.571553506.1628553791; _fbp=fb.1.1628553791262.465845606; _pin_unauth=dWlkPVpHRTJZVEUwTTJJdFlqRTJOUzAwTTJJekxUbG1abVV0Tm1Ga04yVmpOemhqWW1SaQ; _gcl_au=1.1.367746679.1628553792; __hstc=62287155.462d8801b9de513532c892b80fe20422.1628553793504.1628553793504.1628553793504.1; hubspotutk=462d8801b9de513532c892b80fe20422; __hssrc=1; OptanonAlertBoxClosed=2021-08-10T00:03:47.336Z; OptanonConsent=isIABGlobal=false&datestamp=Mon+Aug+09+2021+21%3A03%3A47+GMT-0300+(Argentina+Standard+Time)&version=6.17.0&hosts=&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0002%3A1%2CC0004%3A1&AwaitingReconsent=false; _uetsid=59dbafb0f96e11eb97849f7a3e4dbb25; _uetvid=59dbebc0f96e11eb91d0cf6d2ec058f5; __hssc=62287155.2.1628553793506; _gat_UA-90386801-1=1',
-        }
         if type == "scraping":
             self._scraping()
         if type == "testing":
             self._scraping(testing=True)
         if type == "prescraping":
             self._pre_scraping()
-            self.pre_scraping = True
-            self._scraping()
 
-    
+        
     def _pre_scraping(self):
         token_list = self.get_brand_tokens()
         response_list = _async_requests(token_list,max_workers=1000,session=self.session)
@@ -66,16 +48,16 @@ class Faire:
                     })
         pass
     def _scraping(self,testing=False):
-        response_list = self.test_mongo.search({'site': "Faire"})
-        response_list = list(response_list)
+        response_list = list(self.test_mongo.search({'site': "Faire"}))
         for response in response_list:
-            payload = self.get_payload(response['response'])
-            payload_dict = payload.payload_dict()
-            if payload_dict['Id'] not in self.scraped:
-                print(f"{colors.OKGREEN}[Inserted {payload_dict['Name']} in DB] {colors.ENDC}")
-                self.mongo.InsertOne(payload.payload_dict())
-            else:
-                print(f'{colors.WARNING} [{payload_dict["Name"]} already exists.] Skipping {colors.ENDC}')
+            if response['id'] not in self.scraped:
+                payload = self.get_payload(response['response'])
+                payload_dict = payload.payload_dict()
+                if payload_dict['Id'] not in self.scraped:
+                    print(f"{colors.OKGREEN}[Inserted {payload_dict['Name']} in DB] {colors.ENDC}")
+                    self.mongo.InsertOne(payload.payload_dict())
+                else:
+                    print(f'{colors.WARNING} [{payload_dict["Name"]} already exists.] Skipping {colors.ENDC}')
     def get_brand_tokens(self):
         token_list = []
         list_brands = self.session.get('https://www.faire.com/api/brand/list-brands')
@@ -98,11 +80,9 @@ class Faire:
         payload.id = self.get_id(content_metadata)
         payload.images = self.get_images(content_metadata)
         payload.instagram = self.get_instagram(content_metadata)
-        payload.instagram_followers = self.get_instagram_followers(content_metadata)
         payload.likes = self.get_likes(content_metadata)
         payload.banner_image = self.get_banner_image(content_metadata)
-        payload.facebook_followers = self.get_facebook_followers(content_metadata)
-        payload.facebook_handle = self.get_facebook_handle(content_metadata)
+        payload.facebook = self.get_facebook_handle(content_metadata)
         payload.owner_name = self.get_owner_name(content_metadata)
         payload.hand_made = self.get_hand_made(content_metadata)
         payload.made_in = self.get_made_in(content_metadata)
@@ -173,15 +153,34 @@ class Faire:
             images.append(story_image['url'])
         return images
         pass
-    def get_instagram(self,content_metadata):
+    def get_instagram(self,content_metadata,counter=0):
         if "instagram_handle" in content_metadata['brand']:
             instagram = content_metadata['brand']['instagram_handle']
-            return "@"+instagram
-    def get_instagram_followers(self,content_metadata):
-        if "instagram_followers" in content_metadata['brand']:
-            instagram_followers = content_metadata['brand']['instagram_followers']
-            return instagram_followers
-        pass
+            search_data = self.API.searchUsername(instagram)
+            instagram_json = self.API.LastJson
+            try:
+                user = instagram_json['user']
+                instagram_dict = {
+                    "Username": user['username'],
+                    'FullName': user['full_name'],
+                    'ProfilePic': user['hd_profile_pic_url_info']['url'],
+                    'Bio': user['biography'].replace('\n',''),
+                    'ExternalUrl': user['external_url'] if 'external_url' in user else None,
+                    "Followers": user['follower_count'],
+                    "Following": user['following_count'],
+                    "PublicEmail": user['public_email'] if 'public_email' in user else None,
+                }
+                print(instagram_dict)
+                return instagram_dict
+            except KeyError:
+                if counter == 0:
+                    self.API = InstagramAPI(Private.USER2,Private.PASSWORD2)
+                    self.API.login()
+                    return self.get_instagram(content_metadata,counter=1)
+                else:
+                    raise Exception("Instagram key expired, add a new accoumt or wait until "
+                    + self.regex_time.search(instagram_json['feedback_message']).group())
+                
     def get_name(self,content_metadata):
         name = content_metadata['brand']['name']
         return name
